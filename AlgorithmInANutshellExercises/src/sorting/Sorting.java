0e2ed4268f1a1df209e83088972c47400768ab31
lhaquoc <you@example.com>
2022-03-01 07:16:27 +0700

    edit code
    - Change from tab to spaces
    - New variable name for array

--- AlgorithmInANutshellExercises/src/sorting/Sorting.java
+++ AlgorithmInANutshellExercises/src/sorting/Sorting.java
@@ -1,41 +1,36 @@
 package sorting;
 
 class InsertionSort {
-	public InsertionSort() {
-		// TODO Auto-generated constructor stub
-		System.out.println("InsertionSort");
-	}
-	void sort(int[] A) {
-		for(int i = 1; i <= A.length - 1; i++) {
-			insert(A, i, A[i]);
-		}
-	}
+    public InsertionSort() {
+        System.out.println("InsertionSort");
+    }
 
-	private void insert(int[] A, int pos, int value) {
-		// TODO Auto-generated method stub
-		int i = pos - 1;
-		while(i >= 0 && A[i] > value) {
-			A[i + 1] = A[i];
-			i = i - 1;
-		}
-		A[i + 1] = value;
-	}
+    void sort(int[] inputArray) {
+        for (int i = 1; i <= inputArray.length - 1; i++) {
+	    insert(inputArray, i, inputArray[i]);
+        }
+    }
+    
+    private void insert(int[] array, int pos, int value) {
+        int i = pos - 1;
+        while (i >= 0 && array[i] > value) {
+            array[i + 1] = array[i];
+            i = i - 1;
+        }
+        array[i + 1] = value;
+    }
 }
 
 public class Sorting {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		InsertionSort insertionSort =  new InsertionSort();
-		int[] A = {15, 9, 8, 1, 4, 11, 7, 12, 13, 6, 5, 3, 16, 2, 10, 14};
-		insertionSort.sort(A);
-		print(A);
-	}	
-	
-	private static void print(int[] A) {
-		// TODO Auto-generated method stub
-		for(int i = 0; i <= A.length - 1; i++) {
-			System.out.println(A[i]);
-		}
-	}
+    public static void main(String[] args) {
+        InsertionSort insertionSort = new InsertionSort();
+        int[] array = { 15, 9, 8, 1, 4, 11, 7, 12, 13, 6, 5, 3, 16, 2, 10, 14 };
+        insertionSort.sort(array);
+        print(array);
+    }
+    private static void print(int[] array) {
+	for (int i = 0; i <= array.length - 1; i++) {
+        System.out.println(array[i]);
+        }
+    }
 }
